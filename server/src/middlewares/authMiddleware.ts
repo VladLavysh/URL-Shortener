98b3caba7ca8519b751d85bf38d046aa02ca5c62
Vladislav <miegeff@gmail.com>
2025-02-26 11:37:19 +0100

    deploy fixes 2

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface AuthRequest extends Request {
-  user?: unknown;
+  user?: any;
 }
 
 export const verifyAuthToken = (req: AuthRequest, res: Response, next: NextFunction) => {
